3d74214d664dcd7b4c3060b1dbf6b196f5573e28
mkamalov85 <mkamalov85@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-10 10:09:58 +0000

    Player classes were refactored.

--- dicewars/src/ru/icl/dicewars/sample/AttackPlayer4.java
+++ dicewars/src/ru/icl/dicewars/sample/AttackPlayer4.java
@@ -1,45 +1,8 @@
 package ru.icl.dicewars.sample;
 
-import java.util.Set;
-
-import ru.icl.dicewars.client.Land;
-import ru.icl.dicewars.client.Lead;
-import ru.icl.dicewars.client.Player;
-import ru.icl.dicewars.client.World;
-
-public class AttackPlayer4 implements Player {
+public class AttackPlayer4 extends AbstractAttackPlayer {
 	@Override
 	public String getName() {
 		return "AttackPlayer4";
-	}
-
-	@Override
-	public void init(){
-	}
-
-	@Override
-	public Lead attack(World world) {
-		Set<Land> lands = world.getLands();
-		for (final Land land : lands) {
-			if (land.getFlag().equals(world.getMyFlag()) && land.getDiceCount() > 1) {
-				Set<Land> neighbouringLands = land.getNeighbouringLands();
-				for (final Land neighbouringLand : neighbouringLands) {
-					if (!neighbouringLand.getFlag().equals(world.getMyFlag())) {
-						return new Lead() {
-							@Override
-							public int getFromLandId() {
-								return land.getLandId();
-							}
-
-							@Override
-							public int getToLandId() {
-								return neighbouringLand.getLandId();
-							}
-						};
-					}
-				}
-			}
-		}
-		return null;
 	}
 }
