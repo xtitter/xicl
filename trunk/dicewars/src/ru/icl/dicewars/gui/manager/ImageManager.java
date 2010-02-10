4b5bf2e834e29a15f2507a07d5de536f1f15def2
nantuko84 <nantuko84@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-10 15:20:18 +0000

    added displaying dices (only for 1)

---
+++ trunk/dicewars/src/ru/icl/dicewars/gui/manager/ImageManager.java
@@ -0,0 +1,144 @@
+package ru.icl.dicewars.gui.manager;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import ru.icl.dicewars.util.Transparency;
+
+public class ImageManager {
+
+    protected static Image getImageFromResource(String path) {
+        URL imageURL = ImageManager.class.getResource(path);
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(imageURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+    
+    protected static BufferedImage getBufferedImageFromResource(String path) {
+        URL imageURL = ImageManager.class.getResource(path);
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(imageURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return image;
+    }
+    
+    protected static BufferedImage getBufferedImageFromDisk(String path) {
+        BufferedImage image = null;
+
+        try {
+    		image = ImageIO.read(new File(path));
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+
+        return image;
+    }
+    
+    protected static BufferedImage getBufferedImageFromDisk(String path, Rectangle rec) {
+        BufferedImage image = null;
+        BufferedImage resized = null;
+        
+        try {
+    		image = ImageIO.read(new File(path));
+    		
+    		if (rec == null) {
+                 return image;
+    		}
+
+    		resized = new BufferedImage((int)rec.getWidth(), (int)rec.getHeight(), BufferedImage.TYPE_INT_ARGB);
+    		Graphics2D g = resized.createGraphics();
+    		g.drawImage(image, 0, 0, (int)rec.getWidth(), (int)rec.getHeight(), null);
+    		g.dispose();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+
+        return resized;
+    }
+
+    protected static Image getImageFromResource(String path, Rectangle rec) {
+        URL imageURL = ImageManager.class.getResource(path);
+        BufferedImage image = null;
+        Image resized = null;
+
+        try {
+            image = ImageIO.read(imageURL);
+
+            if (rec == null)
+                return image;
+
+            resized = image.getScaledInstance(rec.width, rec.height, java.awt.Image.SCALE_SMOOTH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return resized;
+    }
+
+    protected static Image getImageFromResourceTransparent(String path, Color mask) {
+        BufferedImage image = null;
+        Image imageCardTransparent = null;
+
+        URL imageURL = ImageManager.class.getResource(path);
+
+        if (imageURL == null) {
+            return null;
+        }
+
+        try {
+            image = ImageIO.read(imageURL);
+            imageCardTransparent = Transparency.makeColorTransparent(image, mask);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return imageCardTransparent;
+    }
+
+    protected static Image getImageFromResourceTransparent(String path, Color mask, Rectangle rec) {
+        BufferedImage image = null;
+        Image imageCardTransparent = null;
+        Image resized = null;
+
+        URL imageURL = ImageManager.class.getResource(path);
+
+        try {
+            image = ImageIO.read(imageURL);
+            imageCardTransparent = Transparency.makeColorTransparent(image, mask);
+
+            resized = imageCardTransparent.getScaledInstance(rec.width, rec.height, java.awt.Image.SCALE_SMOOTH);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return resized;
+    }
+    
+    public static Image getDice1() {
+    	if (dice1 == null) {
+    		dice1 = getImageFromResource("/resources/dice/g1.png");
+    	}
+    	return dice1;
+    }
+    
+    private static Image dice1;
+
+}
