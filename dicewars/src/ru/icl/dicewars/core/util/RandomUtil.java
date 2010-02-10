96ecf8091f9944e0dca8aa73372073bf0ef3c01e
mkamalov85 <mkamalov85@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-10 10:44:30 +0000

    Encapsulation to util class was added. Util class can't be instantiated.

--- dicewars/src/ru/icl/dicewars/core/util/RandomUtil.java
+++ dicewars/src/ru/icl/dicewars/core/util/RandomUtil.java
@@ -5,6 +5,9 @@ import java.util.Random;
 public class RandomUtil {
 	public static final Random rnd = new Random(System.currentTimeMillis());
 	
+	private RandomUtil() {
+	}
+	
 	public static int getRandomInt(int n){
 		return rnd.nextInt(n);
 	}
