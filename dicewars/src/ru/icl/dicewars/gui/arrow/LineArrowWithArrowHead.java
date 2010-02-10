b425fbc3067528091425fac67fe0b4f2396d4c9b
nantuko84 <nantuko84@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-10 13:29:03 +0000

    added arrow shadow. as bezier lines don't work for some cases, replaced them by dashed lines for a while.

--- dicewars/src/ru/icl/dicewars/gui/arrow/LineArrowWithArrowHead.java
+++ dicewars/src/ru/icl/dicewars/gui/arrow/LineArrowWithArrowHead.java
@@ -4,7 +4,6 @@ import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
-import java.awt.Rectangle;
 
 public class LineArrowWithArrowHead extends LineArrow {
 
@@ -26,24 +25,40 @@ public class LineArrowWithArrowHead extends LineArrow {
 		}
 	}
 
-	public static void drawArrowHead(Graphics2D g2d, int xCenter, int yCenter, int x, int y) {
+	public void drawArrowHead(Graphics2D g2d, int xCenter, int yCenter, int x, int y) {
 		double aDir = Math.atan2(xCenter - x, yCenter - y);
 		g2d.setStroke(new BasicStroke(1f));
 		Polygon tmpPoly = new Polygon();
+		Polygon tmpPoly2 = new Polygon();
 		int i1 = 12 + (int) (arrowSize * 2);
 		int i2 = 6 + (int) arrowSize;
 		tmpPoly.addPoint(x, y);
+		tmpPoly2.addPoint(x+1, y+2);
 		tmpPoly.addPoint(x + xCor(i1, aDir + .5), y + yCor(i1, aDir + .5));
-		Rectangle r1 = new Rectangle(x, y, x + xCor(i1, aDir + .5), y + yCor(i1, aDir + .5));
+		tmpPoly2.addPoint(x + xCor(i1, aDir + .5)+1, y + yCor(i1, aDir + .5)+2);
+		//Rectangle r1 = new Rectangle(x, y, x + xCor(i1, aDir + .5), y + yCor(i1, aDir + .5));
 		tmpPoly.addPoint(x + xCor(i2, aDir), y + yCor(i2, aDir));
 		tmpPoly.addPoint(x + xCor(i1, aDir - .5), y + yCor(i1, aDir - .5));
 		tmpPoly.addPoint(x, y);
-		Rectangle r2 = new Rectangle(x + xCor(i1, aDir - .5), y + yCor(i1, aDir - .5), x, y);
+		
+		tmpPoly2.addPoint(x + xCor(i2, aDir)+1, y + yCor(i2, aDir)+2);
+		tmpPoly2.addPoint(x + xCor(i1, aDir - .5)+1, y + yCor(i1, aDir - .5)+2);
+		tmpPoly2.addPoint(x+1, y+2);
+		//Rectangle r2 = new Rectangle(x + xCor(i1, aDir - .5), y + yCor(i1, aDir - .5), x, y);
+		
+		g2d.setColor(LineArrow.shadowColor);
+		
+		g2d.drawPolygon(tmpPoly2);
+		g2d.fillPolygon(tmpPoly2);
+		
+		g2d.setColor(super.color);
+		
 		g2d.drawPolygon(tmpPoly);
 		g2d.fillPolygon(tmpPoly);
-		//g2d.setColor(Color.black);
-		g2d.drawLine(r1.x, r1.y, r1.width, r1.height);
-		g2d.drawLine(r2.x, r2.y, r2.width, r2.height);
+		
+		//g2d.setColor(LineArrow.shadowColor);
+		//g2d.drawLine(r1.x, r1.y, r1.width, r1.height);
+		//g2d.drawLine(r2.x, r2.y, r2.width, r2.height);
 	}
 
 	private static int yCor(int len, double dir) {
