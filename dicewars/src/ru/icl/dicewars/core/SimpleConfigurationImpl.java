53849e3b9310981f38d6e9fd96cce2aff77f7e50
mkamalov85 <mkamalov85@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-13 09:24:41 +0000

    Player class loading mechanism was changed. Now application loads classes from configuration.properties and all classes from "player" folder that implements Player interface.

--- dicewars/src/ru/icl/dicewars/core/SimpleConfigurationImpl.java
+++ dicewars/src/ru/icl/dicewars/core/SimpleConfigurationImpl.java
@@ -1,16 +1,31 @@
 package ru.icl.dicewars.core;
 
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Properties;
+import java.util.Queue;
+import java.util.Set;
 
+import org.objectweb.asm.ClassReader;
+
+import ru.icl.dicewars.client.Player;
 import ru.icl.dicewars.core.exception.InvalidConfigurationLoadingExecption;
 
 public class SimpleConfigurationImpl implements Configuration {
-	private final static String[] CONFIG_FILE_LOCATIONS = new String[] {
-			"configuration.properties", "players.properties" };
-	private final static String PLAYER_CLASSES = "players";
-	private final static String MAX_DICE_COUNT_IN_RESERVE = "maxDiceCountInReserve";
+	private final static String[] CONFIG_FILE_LOCATIONS = new String[] { "configuration.properties" };
+	
+	private final static String DEFAULT_PLAYER_PROPERTY_NAME = "default_players";
+	
+	private final static String PLAYERS_SCAN_DIR_PROPERTY_NAME = "players_scan_dir";
+	
+	private final static String MAX_DICE_COUNT_IN_RESERVE_PROPERTY_NAME = "max_dice_in_reserve";
+	
+	private final static String DEFAULT_PLAYER_SCAN_DIR = "players"; 
 	
 	private static final int DEFAULT_MAX_DICE_COUNT_IN_RESERVE = 64;
 
@@ -21,24 +36,77 @@ public class SimpleConfigurationImpl implements Configuration {
 	private FullWorldGenerator fullWorldGenerator = null;
 	private Object flag = new Object();
 	private Object flag2 = new Object();
+	private String playerScanDir = DEFAULT_PLAYER_SCAN_DIR;
 
 	public SimpleConfigurationImpl() {
 		Properties properties = new Properties();
-		try {
-			for (int i = 0; i < CONFIG_FILE_LOCATIONS.length; i++) {
-				properties.load(new FileReader(new File(
-						CONFIG_FILE_LOCATIONS[i])));
+		for (int i = 0; i < CONFIG_FILE_LOCATIONS.length; i++) {
+			InputStream is = null;
+			try{
+				is = this.getClass().getClassLoader().getResourceAsStream(CONFIG_FILE_LOCATIONS[i]);
+				properties.load(new InputStreamReader(is));
+			}catch (IOException e) {
+				throw new InvalidConfigurationLoadingExecption(e);
+			}finally{
+				if (is != null){
+					try{
+						is.close();
+					}catch (IOException e) {
+					}
+				}
 			}
-		} catch (Exception e) {
-			throw new InvalidConfigurationLoadingExecption(e);
 		}
-		String players = properties.getProperty(PLAYER_CLASSES);
-		String[] classNames = players.split(",");
-		int playersCount = classNames.length;
-		this.playersCount = playersCount;
-		this.classNames = classNames;
+
+		String players = properties.getProperty(DEFAULT_PLAYER_PROPERTY_NAME);
+		String[] defaultPlayerClassNames = players.split(",");
+		
+		playerScanDir = properties.getProperty(PLAYERS_SCAN_DIR_PROPERTY_NAME);
+		
+		File dir = new File(playerScanDir);
+		
+		Set<String> classNames = new HashSet<String>();
+		
+		for (String className : defaultPlayerClassNames){
+			classNames.add(className);
+		}
+		
+		if (dir.isDirectory()){
+			File[] files = dir.listFiles();
+			Queue<File> queue = new LinkedList<File>();
+			for (File file : files){
+				queue.add(file);
+			}
+			while (!queue.isEmpty()){
+				File file = queue.poll();
+				if (file.isDirectory()){
+					File[] fs = file.listFiles();
+					for (File f : fs){
+						queue.add(f);
+					}		
+				}else{
+					try{
+						ClassReader cr = new ClassReader(new FileInputStream(file));
+						Boolean f = false;
+						if (cr.getInterfaces().length > 0){
+							for (String s : cr.getInterfaces()){
+								if (s.replaceAll("/", ".").equals(Player.class.getName())){
+									f = true;
+								}
+							}
+						}
+						if (f)
+							classNames.add(cr.getClassName().replaceAll("/", "."));
+					}catch (IOException e) {
+					}
+				}
+			}
+		}
+		
+		this.classNames = classNames.toArray(new String[]{});
+		this.playersCount = classNames.size();
+		
 		try{
-			this.maxDiceCountInReserve = Integer.valueOf(properties.getProperty(MAX_DICE_COUNT_IN_RESERVE));
+			this.maxDiceCountInReserve = Integer.valueOf(properties.getProperty(MAX_DICE_COUNT_IN_RESERVE_PROPERTY_NAME));
 		}catch (NumberFormatException e) {
 		}
 	}
@@ -48,9 +116,9 @@ public class SimpleConfigurationImpl implements Configuration {
 		if (fullWorldGenerator == null) {
 			synchronized (flag2) {
 				if (fullWorldGenerator == null) {
-                    RealFullWorldGeneratorImpl generator = new RealFullWorldGeneratorImpl();
-                    generator.setPlayersCount(playersCount);
-                    return generator;
+					RealFullWorldGeneratorImpl generator = new RealFullWorldGeneratorImpl();
+					generator.setPlayersCount(playersCount);
+					return generator;
 				}
 			}
 		}
@@ -74,7 +142,7 @@ public class SimpleConfigurationImpl implements Configuration {
 	public int getPlayersCount() {
 		return playersCount;
 	}
-	
+
 	@Override
 	public int getMaxDiceCountInReserve() {
 		return maxDiceCountInReserve;
--- dicewars/tools/run.bat
+++ dicewars/tools/run.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=%CLASSPATH%;./dicewars.jar;./dicewars-client.jar
+set CLASSPATH=%CLASSPATH%;./dicewars.jar;./dicewars-client.jar;asm-3.1.jar;./players
 java ru.icl.dicewars.DiceWars
