3f9ddb893359f5bbd1687e4292f0c4912b8f5587
mkamalov85 <mkamalov85@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-14 09:56:30 +0000

    Debug feature was added. Transport: dt_socket, Port: 8000.

--- trunk/dicewars/tools/run.bat
+++ trunk/dicewars/tools/run.bat
@@ -1,2 +1,7 @@
 set CLASSPATH=%CLASSPATH%;./dicewars.jar;./dicewars-client.jar;asm-3.1.jar;./players
-java ru.icl.dicewars.DiceWars
+set JPDA_TRANSPORT=dt_socket
+set JPDA_ADDRESS=8000
+set JPDA_SUSPEND=n
+set JPDA_OPTS=-agentlib:jdwp=transport=%JPDA_TRANSPORT%,address=%JPDA_ADDRESS%,server=y,suspend=%JPDA_SUSPEND%
+
+java %JPDA_OPTS% ru.icl.dicewars.DiceWars
