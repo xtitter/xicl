2a65ca695faebf78e7ca9932abc1c7d9bdac9a86
mkamalov85 <mkamalov85@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-18 11:52:29 +0000

    Added final statement to constant field.

--- trunk/dicewars/src/ru/icl/dicewars/gui/component/Command.java
+++ trunk/dicewars/src/ru/icl/dicewars/gui/component/Command.java
@@ -2,8 +2,9 @@ package ru.icl.dicewars.gui.component;
 
 
 public interface Command {
-    public static Command Blank = new Command() {
+    public final static Command BLANK = new Command() {
         public void execute() {}
     };
+    
     public void execute();
 }
