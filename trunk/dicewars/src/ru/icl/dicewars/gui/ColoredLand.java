f220dc7db0840692c613e7ac9212c1996a18987e
nantuko84 <nantuko84@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-18 06:39:31 +0000

    returned back dices. changed painting order (caused image overlapping)

--- trunk/dicewars/src/ru/icl/dicewars/gui/ColoredLand.java
+++ trunk/dicewars/src/ru/icl/dicewars/gui/ColoredLand.java
@@ -1,14 +1,13 @@
 package ru.icl.dicewars.gui;
 
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 
-import ru.icl.dicewars.core.FullLand;
-
 public class ColoredLand {
-	public FullLand land;
 	public Dimension size;
 	public int x;
 	public int y;
 	public BufferedImage image;
+	public Point center;
 }
