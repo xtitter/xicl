d6bc520859a45e1d7830eace63626ec608e7dc98
mkamalov85 <mkamalov85@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-02-25 21:10:28 +0000

    Refactoring

--- trunk/dicewars/src/ru/icl/dicewars/gui/ColoredLand.java
+++ trunk/dicewars/src/ru/icl/dicewars/gui/ColoredLand.java
@@ -4,10 +4,51 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
 
-public class ColoredLand {
+class ColoredLand {
 	public Dimension size;
 	public int x;
 	public int y;
 	public BufferedImage image;
 	public Point center;
+
+	public Dimension getSize() {
+		return size;
+	}
+
+	public void setSize(Dimension size) {
+		this.size = size;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+	public Point getCenter() {
+		return center;
+	}
+
+	public void setCenter(Point center) {
+		this.center = center;
+	}
+
 }
--- trunk/dicewars/src/ru/icl/dicewars/gui/LandFactory.java
+++ trunk/dicewars/src/ru/icl/dicewars/gui/LandFactory.java
@@ -9,39 +9,40 @@ import ru.icl.dicewars.core.FullLandImpl;
 import ru.icl.dicewars.core.FullWorld;
 import ru.icl.dicewars.core.Point;
 
-public class LandFactory {
+class LandFactory {
 
-	private static HashMap<Integer, LandPainter> landPainters = new HashMap<Integer, LandPainter>();
-	
-	public static ColoredLand getLand(int landId, Flag flag) {
+	private HashMap<Integer, LandPainter> landPainters = new HashMap<Integer, LandPainter>();
+
+	ColoredLand getLand(int landId, Flag flag) {
 		if (landPainters.containsKey(Integer.valueOf(landId))) {
 			return landPainters.get(Integer.valueOf(landId)).getLand(flag);
 		}
 		return null;
 	}
-	
-	public static void buildTheWorld(FullWorld world) {
+
+	void buildTheWorld(FullWorld world) {
 		for (FullLand land : world.getFullLands()) {
 			LandPainter landPainter = new LandPainter(land, world.getFlags());
 			landPainters.put(land.getLandId(), landPainter);
 		}
 	}
-	
-	public static void buildBackground(FullWorld world) {
+
+	void buildBackground(FullWorld world) {
 		FullLand empty = new FullLandImpl(0);
 		for (int i = WorldJPanel.MIN_X; i < WorldJPanel.MAX_X + 1; i++) {
 			for (int j = WorldJPanel.MIN_Y; j < WorldJPanel.MAX_Y + 1; j++) {
-				empty.getPoints().add(new Point(i,j));
+				empty.getPoints().add(new Point(i, j));
 			}
 		}
 		for (FullLand land : world.getFullLands()) {
 			empty.getPoints().removeAll(land.getPoints());
 		}
-		LandPainter landPainter = new LandPainter(empty, new Color(240,240,240,150));
+		LandPainter landPainter = new LandPainter(empty, new Color(240, 240,
+				240, 150));
 		landPainters.put(0, landPainter);
 	}
-	
-	public static ColoredLand getBackground() {
+
+	ColoredLand getBackground() {
 		if (landPainters.containsKey(Integer.valueOf(0))) {
 			return landPainters.get(Integer.valueOf(0)).getBackground();
 		}
