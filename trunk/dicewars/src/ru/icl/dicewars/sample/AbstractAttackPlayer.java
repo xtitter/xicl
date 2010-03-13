ffc10ea35bab7c17a0616cb44cd0838bf0dbca16
mkamalov85 <mkamalov85@2de32f76-1620-11df-80a0-7b270d503f7e>
2010-03-13 23:58:10 +0000

    AttackBots were made more simple.

--- trunk/dicewars/src/ru/icl/dicewars/sample/AbstractAttackPlayer.java
+++ trunk/dicewars/src/ru/icl/dicewars/sample/AbstractAttackPlayer.java
@@ -2,9 +2,9 @@ package ru.icl.dicewars.sample;
 
 import java.util.Set;
 
+import ru.icl.dicewars.client.Attack;
 import ru.icl.dicewars.client.Flag;
 import ru.icl.dicewars.client.Land;
-import ru.icl.dicewars.client.Attack;
 import ru.icl.dicewars.client.Player;
 import ru.icl.dicewars.client.World;
 
@@ -25,26 +25,6 @@ public abstract class AbstractAttackPlayer implements Player{
 	@Override
 	public Attack attack(World world) {
 		Set<Land> lands = world.getLands();
-		for (final Land land : lands) {
-			if (land.getFlag().equals(world.getMyFlag()) && land.getDiceCount() > 1) {
-				Set<Land> neighbouringLands = land.getNeighbouringLands();
-				for (final Land neighbouringLand : neighbouringLands) {
-					if (!neighbouringLand.getFlag().equals(world.getMyFlag()) && land.getDiceCount() > neighbouringLand.getDiceCount()) {
-						return new Attack() {
-							@Override
-							public int getFromLandId() {
-								return land.getLandId();
-							}
-
-							@Override
-							public int getToLandId() {
-								return neighbouringLand.getLandId();
-							}
-						};
-					}
-				}
-			}
-		}
 		for (final Land land : lands) {
 			if (land.getFlag().equals(world.getMyFlag()) && land.getDiceCount() > 1) {
 				Set<Land> neighbouringLands = land.getNeighbouringLands();
